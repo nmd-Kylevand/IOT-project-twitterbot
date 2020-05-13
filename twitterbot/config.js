f91070e7ccd01517103227491ac4a6dc55b5ecda
nmdgent-gillpera <gillian.peraltagutierrez@student.arteveldehs.be>
2020-05-13 14:20:42 +0200

    Update config.js

--- twitterbot/config.js
+++ twitterbot/config.js
@@ -1,7 +1,7 @@
 var config = {
- consumer_key:         'XXXXX',
- consumer_secret:      'XXXXX',
- access_token:         'XXXXX',
-  access_token_secret:  'XXXXX'
+ consumer_key:         'gMVXF5ljaqONMjC0PbolteeTw',
+  consumer_secret:      'zH9JRTCQ0P6idLR0ScOh8OpvKMHB',
+ access_token:         '1258397035306332169-DR6TNZaQ60jJx4RhaLidsknIuGaBqs',
+  access_token_secret:  'HCs3wgypc68neUQSJRkjCmgzUJm4yEWkdgAEXJCQ1Gdmu'
 }
 module.exports = config;
