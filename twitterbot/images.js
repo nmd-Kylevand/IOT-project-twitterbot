3b966414e536b5e4e5b30d4ddd2951f92829607c
nmd-Kylevand <kyle.vandeputte@student.arteveldehs.be>
2020-05-14 17:34:37 +0200

    Final groundwork

--- twitterbot/images.js
+++ twitterbot/images.js
@@ -1,12 +1,11 @@
-
 var images = [
   {
     file: 'image0001.jpg',
-    source: 'https://nl.wikipedia.org/wiki/Great_Race_of_Mercy'
+    source:   'Wh but also the leap into electronic typesetting, remaining essentially unchanged. It was populari with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum' + 'https://commons.wikimedia.org/wiki/File:Balto.jpg'
   },
  {
     file: 'imafge0001.jpg',
-    source: 'https://nl.wikipedia.org/wiki/Great_Race_of_Mercy'
-  },
-]
+    source: 'What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem  with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum' + 'https://nl.wikipedia.org/wiki/Great_Race_of_Mercy'  
+ }
+];
 module.exports = images;
--- twitterbot/server.js
+++ twitterbot/server.js
@@ -7,18 +7,21 @@ var fs = require('fs'),
 
     config = require(path.join(__dirname, 'config.js'));console.log('I am a Twitter bot!');
     
-    images = require(path.join(__dirname, 'images.js'));
-
+  
 var T = new Twit(config);
+const images = require(path.join(__dirname, 'images.js'));
+const image = random_from_array(images);
 
 
 
 function random_from_array(images){
   return images[Math.floor(Math.random() * images.length)];
 }
+
+
 function upload_random_image(images){
   console.log('Opening an image...');
-  var image_path = path.join(__dirname, '/images/' + random_from_array(images)),
+  var image_path = path.join(__dirname, '/images/' + image.file),
       b64content = fs.readFileSync(image_path, { encoding: 'base64' });
 
   console.log('Uploading an image...');
@@ -32,7 +35,7 @@ function upload_random_image(images){
       console.log('Image uploaded!');
       console.log('Now tweeting it...');
 
-      T.post('statuses/update', {
+      T.post('statuses/update', { status: (image.source),
           media_ids: new Array(data.media_id_string)
         },
         function(err, data, response) {
